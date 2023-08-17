bff4154f22f29e4c460ef9ad29049e9966218ac9
Hirad Arshadi <hiradwork@gmail.com>
2023-08-16 23:12:31 -0700

    temp: suppressing eslint on builds

--- next.config.js
+++ next.config.js
@@ -26,6 +26,11 @@ const defaultNextConfig = {
       exclude: ['error'],
     },
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'src/styles')],
   },
